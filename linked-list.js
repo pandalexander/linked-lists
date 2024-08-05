8ccaa8b21f7603de9a7a3b401833ba0f25371796
Alex Seeley <alexseeley134@gmail.com>
2024-08-04 20:48:51 -0600

    Add list, node, and append function to environment

--- linked-list.js
+++ linked-list.js
@@ -1,1 +1,41 @@
-console.log("HIYA!");
+function linkedList(head = null) {
+  this.head = head;
+
+  function append(value) {
+    let newNode = new node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+
+    if (current.nextNode === null) {
+      current.nextNode = newNode;
+    } else {
+      while (current.nextNode !== null) {
+        current = current.newNode;
+      }
+      current.nextNode = newNode;
+    }
+  }
+
+  return { head, append };
+}
+
+function node(value = null, nextNode = null) {
+  this.value = value;
+  this.nextNode = nextNode;
+
+  return { value, nextNode };
+}
+
+const defaultList = new linkedList();
+
+console.log(defaultList);
+
+defaultList.append("foo");
+
+defaultList.append("bar");
+
+console.log(defaultList);

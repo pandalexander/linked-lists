4b0115ad69ee53442552a30ed0d2810549cb16e2
Alex Seeley <alexseeley134@gmail.com>
2024-08-05 20:04:45 -0600

    Add size function

--- linked-list.js
+++ linked-list.js
@@ -26,7 +26,24 @@ function linkedList(head = null) {
     this.head = newNode;
   }
 
-  return { head, append, prepend };
+  function size() {
+    let count = 0;
+
+    if (this.head === null) {
+      return 0;
+    } else {
+      count = 1;
+      let current = this.head;
+      while (current.nextNode !== null) {
+        current = current.nextNode;
+        count++;
+      }
+    }
+
+    return count;
+  }
+
+  return { head, append, prepend, size };
 }
 
 function node(value = null, nextNode = null) {
@@ -37,3 +54,27 @@ function node(value = null, nextNode = null) {
 }
 
 const defaultList = new linkedList();
+
+console.log(defaultList);
+
+console.log("");
+
+console.log(defaultList.size());
+
+defaultList.append("buzz");
+
+defaultList.prepend("fizz");
+
+console.log(defaultList.size());
+
+defaultList.prepend("first");
+
+defaultList.append("last");
+
+console.log("");
+
+console.log(defaultList);
+
+console.log("");
+
+console.log(defaultList.size());
